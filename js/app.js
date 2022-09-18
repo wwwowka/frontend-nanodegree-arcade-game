a69f9f24dbddb2774e250398790d2516e6c3d5a6
Volodymyr Vyshnyakov <wwwowka@gmail.com>
2022-09-18 19:01:21 +0300

    it's a working game Frogger

--- js/app.js
+++ js/app.js
@@ -16,72 +16,75 @@ const playerStarting = {
     pause: 500
 }
 
-var Enemy = function (x, y, speed) {
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.sprite = 'images/enemy-bug.png';
-};
-
-Enemy.prototype.update = function (dt) {
-    this.x += this.speed * dt;
-
-    if (this.x > playArea.width) { 
-
-        this.x = -(playArea.gridWidth);
-        this.speed = (Math.random() * (enemy.maxSpeed + enemy.minSpeed) + enemy.minSpeed);
+class Enemy {
+    constructor(x, y, speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.sprite = 'images/enemy-bug.png';
     }
+    update(dt) {
+        this.x += this.speed * dt;
 
-    if (player.x < this.x + playArea.gridWidth &&
-        player.x + playArea.gridWidth > this.x &&
-        player.y < this.y + playArea.gridHeight &&
-        playArea.gridHeight + player.y > this.y) {
+        if (this.x > playArea.width) {
 
-        player.x = playerStarting.x;
-        player.y = playerStarting.y;
+            this.x = -(playArea.gridWidth);
+            this.speed = (Math.random() * (enemy.maxSpeed + enemy.minSpeed) + enemy.minSpeed);
+        }
 
-    }
-};
+        if (player.x < this.x + playArea.gridWidth &&
+            player.x + playArea.gridWidth > this.x &&
+            player.y < this.y + playArea.gridHeight &&
+            playArea.gridHeight + player.y > this.y) {
 
-Enemy.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-
-let Player = function (x, y) {
-    this.x = x;
-    this.y = y;
-    this.player = 'images/char-boy.png'
-};
-
-Player.prototype.update = function (dt) {
-
-};
-
-Player.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.player), this.x, this.y);
-};
-
-Player.prototype.handleInput = function (keyPress) {
-    if (keyPress == 'left' && this.x >= playArea.gridWidth) {
-        this.x -= playArea.gridWidth;
-    }
-    if (keyPress == 'right' && this.x < playArea.width - playArea.gridWidth) {
-        this.x += playArea.gridWidth;
-    }
-    if (keyPress == 'up' && this.y > playArea.gridHeight -  playArea.gridHeight) {
-        this.y -= playArea.gridHeight;
-    }
-    if (keyPress == 'down' && this.y < playerStarting.y) {
-        this.y += playArea.gridHeight;
-    }
-    if (this.y < (playArea.gridHeight - playArea.gridHeight)) {
-        setTimeout(function () {
             player.x = playerStarting.x;
             player.y = playerStarting.y;
-        }, playerStarting.pause);
+
+        }
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
+
+
+
+
+class Player {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.player = 'images/char-boy.png';
+    }
+    update(dt) {
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.player), this.x, this.y);
+    }
+    handleInput(keyPress) {
+        if (keyPress == 'left' && this.x >= playArea.gridWidth) {
+            this.x -= playArea.gridWidth;
+        }
+        if (keyPress == 'right' && this.x < playArea.width - playArea.gridWidth) {
+            this.x += playArea.gridWidth;
+        }
+        if (keyPress == 'up' && this.y > playArea.gridHeight - playArea.gridHeight) {
+            this.y -= playArea.gridHeight;
+        }
+        if (keyPress == 'down' && this.y < playerStarting.y) {
+            this.y += playArea.gridHeight;
+        }
+        if (this.y < (playArea.gridHeight - playArea.gridHeight)) {
+            setTimeout(function () {
+                player.x = playerStarting.x;
+                player.y = playerStarting.y;
+            }, playerStarting.pause);
+        }
+    }
+}
+
+
+
 
 let allEnemies = [];
 
